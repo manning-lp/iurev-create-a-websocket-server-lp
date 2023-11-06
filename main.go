f6c273fa766ab8bad05dc85d02bbb5d138b7519f
Vitalii Yulieff <iurev@evilmartians.com>
2023-11-06 17:34:00 +0400

    wip

--- main.go
+++ main.go
@@ -2,19 +2,47 @@ package main
 
 // todo: learn what are packages
 // todo: learn named imports
+// todo: learn about passing functions into functions. Are they copied or passed by reference?
+// todo: learn http.Error status int
 
 import (
 	"fmt"
 	"net/http"
+  "strings"
+  "github.com/gorilla/websocket"
 )
 
+const welcomeMessage = "Welcome to support. My name is Rheo. How can I help you today?"
+
+var upgrader = websocket.Upgrader{}
 
 func handler(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(writer, "Hello World, %s", request.URL.Path[:1])
+  conn, err := upgrader.Upgrade(writer, request, nil)
+  if err != nil {
+    fmt.Println("ws connection upgrade failed", err)
+    http.Error(writer, "ws connection upgrade failed", 1)
+    return
+  }
+  // messageType, p, err := conn.ReadMessage()
+  conn.WriteMessage(websocket.TextMessage, []byte(welcomeMessage));
+
+  for {
+    _, bMessage, err := conn.ReadMessage()
+    message := string(bMessage[:])
+    
+    if err != nil {
+      fmt.Println("err")
+    }
+    resMessage := strings.ToUpper(message)
+    conn.WriteMessage(websocket.TextMessage, []byte(resMessage));
+    fmt.Println(message)
+  }
+  fmt.Println("ws connection upgraded succesfully")
 }
 
 func main() {
   http.Handle("/", http.FileServer(http.Dir("./milestone1")))
-	// http.HandleFunc("/", handler) // todo: learn about passing functions into functions. Are they copied or passed by reference?
+	http.HandleFunc("/chat", handler)
+
 	http.ListenAndServe(":8080", nil) // todo: learn about second arg
 }
